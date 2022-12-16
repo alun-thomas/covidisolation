2d344494f63d118a2d59546a4509d1a3d1db096c
alun.thomas@utah.edu <alun.thomas@utah.edu>
2022-12-16 14:26:05 -0700

    publish

--- isol.R
+++ isol.R
@@ -1,13 +1,14 @@
 source("covid.R")
 library(xtable,quietly=TRUE)
-
 DMod = makeModel(deltaParams())
 OPar = omicronParams()
 OMod = makeModel(OPar)
+PCRsens = 0.95
+ANTIsens = 0.90
 
 print(xtable(gammas(DMod),
 	caption="Gamma time-in-state distribution parameters and summary
-		 values for original \\covid variants.\\bigskip",
+		 values for the original \\covid variants.\\bigskip",
 	digits=c(0,0,0,2,3,2,2,2,2),
 	label="deltagammas"),
 	include.rownames=FALSE,
@@ -15,7 +16,8 @@ print(xtable(gammas(DMod),
 	)
 
 print(xtable(probs(DMod),
-	caption="Path probability parameters for original \\covid variants.",
+	caption="Probability parameter values specifying the path through the 
+		course of infection for the original \\covid variants.\\bigskip",
 	label="deltaprobs"),
 	include.rownames=FALSE,
 	caption.placement="top"
@@ -31,34 +33,42 @@ print(xtable(gammas(OMod),
 	)
 
 x = (0:100)/4
+pinf = 0.7
+par(mfrow=c(2,2))
+pileup(x,StateProbs(x,pinf,DMod))
+title("Unconditional")
+pileup(x,ConditionalStateProbs(x,pinf,DMod,NoSymptoms))
+title("No symptoms")
+pileup(x,ConditionalStateProbs(x,pinf,DMod,NoSymptoms*PcrTest(PCRsens)))
+title("No symptoms and negative PCR test")
+pileup(x,ConditionalStateProbs(x,pinf,DMod,NoSymptoms*AntigenTest(ANTIsens)))
+title("No symptoms and negative antigen test")
 
-pileup(x,StateProbs(x,0.2,DMod))
-
-pileup(x,ConditionalStateProbs(x,1,DMod,NoSymptoms))
-
-pileup(x,ConditionalStateProbs(x,1,DMod,NoSymptoms*PcrTest(0.95)))
-
-pileup(x,ConditionalStateProbs(x,1,DMod,NoSymptoms*AntigenTest(0.9)))
-
+par(mfrow=c(2,1))
 x = (0:100)/4
+prior = c(1,9)/10
+prior = (1:9)/10
+cols = colorRampPalette(c("blue","red"))(length(prior))
+
 frame(x)
-#for (p in (1:9)/10)
-for (p in c(1,9)/10)
+title("Probability that an infection occurred given negative antigen test")
+for (i in 1:length(prior))
 {
-	q = PostProbTransmission(x,p,AntigenTest(0.9),DMod)
-	lines(x,q)
+	p = prior[i]
+	q = PostProbTransmission(x,p,AntigenTest(ANTIsens),DMod)
+	lines(x,q,col=cols[i])
 	z = x[q==min(q)]
 	abline(v=z)
 	text(z,0,paste(z))
 }
 
-x = (0:100)/4
 frame(x)
-#for (p in (1:9)/10)
-for (p in c(1,9)/10)
+title("Probability that an infection occurred given negative PCR test")
+for (i in 1:length(prior))
 {
-	q = PostProbTransmission(x,p,PcrTest(0.95),DMod)
-	lines(x,q)
+	p = prior[i]
+	q = PostProbTransmission(x,p,PcrTest(PCRsens),DMod)
+	lines(x,q,col=cols[i])
 	z = x[q==min(q)]
 	abline(v=z)
 	text(z,0,paste(z))
