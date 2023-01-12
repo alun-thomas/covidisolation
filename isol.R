9de703e6e67e08d5ca9d089f2e31667dd1878d5d
alun.thomas@utah.edu <alun.thomas@utah.edu>
2023-01-12 16:47:48 -0700

    publish

--- isol.R
+++ isol.R
@@ -7,6 +7,11 @@ PCRsens = 0.95
 ANTIsens = 0.90
 x = (0:100)/4
 
+pc = function(x,d=0)
+{
+	round(x*100,d)
+}
+
 # Function to make table of Gamma parameters.
 gammas = function(Q)
 {
@@ -25,7 +30,7 @@ gammas = function(Q)
 # Function to make table of path splitting parameters.
 probs = function(Q)
 {
-	Name = c("Symptomatic","Svrere")
+	Name = c("Symptomatic","Severe")
 	Parameter = c("q","r")
 	Value = c(Q$q,Q$r)
 	data.frame(Parameter,Name,Value)
@@ -57,31 +62,43 @@ print(xtable(gammas(OMod),
 	caption.placement="top"
 	)
 
-x = (0:100)/4
 pinf = 0.7
+
+x = (0:100)/4
 par(mfrow=c(2,2))
 pileup(x,StateProbs(x,pinf,DMod))
 title("Unconditional")
 pileup(x,ConditionalStateProbs(x,pinf,DMod,NoSymptoms))
 title("No symptoms")
-pileup(x,ConditionalStateProbs(x,pinf,DMod,NoSymptoms*PcrTest(PCRsens)))
-title("No symptoms and negative PCR test")
 pileup(x,ConditionalStateProbs(x,pinf,DMod,NoSymptoms*AntigenTest(ANTIsens)))
 title("No symptoms and negative antigen test")
+pileup(x,ConditionalStateProbs(x,pinf,DMod,NoSymptoms*PcrTest(PCRsens)))
+title("No symptoms and negative PCR test")
 
-par(mfrow=c(2,1))
+x = (0:100)/4
+par(mfrow=c(2,2))
+pileup(x,StateProbs(x,pinf,OMod))
+title("Unconditional")
+pileup(x,ConditionalStateProbs(x,pinf,OMod,NoSymptoms))
+title("No symptoms")
+pileup(x,ConditionalStateProbs(x,pinf,OMod,NoSymptoms*AntigenTest(ANTIsens)))
+title("No symptoms and negative antigen test")
+pileup(x,ConditionalStateProbs(x,pinf,OMod,NoSymptoms*PcrTest(PCRsens)))
+title("No symptoms and negative PCR test")
+
 x = (0:100)/4
 #prior = c(1,9)/10
-#prior = 5/10
 prior = (1:9)/10
 cols = colorRampPalette(c("blue","red"))(length(prior))
+par(mfcol=c(2,2))
 
+mod = DMod
 frame(x)
-title("Probability that an infection occurred given negative antigen test")
+title("Delta variant antigen test")
 for (i in 1:length(prior))
 {
 	p = prior[i]
-	q = PostProbTransmission(x,p,AntigenTest(ANTIsens),DMod)
+	q = PostProbTransmission(x,p,AntigenTest(ANTIsens),mod)
 	lines(x,q,col=cols[i])
 	z = x[q==min(q)]
 	abline(v=z)
@@ -89,14 +106,103 @@ for (i in 1:length(prior))
 }
 
 frame(x)
-title("Probability that an infection occurred given negative PCR test")
+title("Delta variant PCR test")
 for (i in 1:length(prior))
 {
 	p = prior[i]
-	q = PostProbTransmission(x,p,PcrTest(PCRsens),DMod)
+	q = PostProbTransmission(x,p,PcrTest(PCRsens),mod)
 	lines(x,q,col=cols[i])
 	z = x[q==min(q)]
 	abline(v=z)
 	text(z,0,paste(z))
 }
+
+mod = OMod
+
+frame(x)
+title("Omicron variant antigen test")
+for (i in 1:length(prior))
+{
+	p = prior[i]
+	q = PostProbTransmission(x,p,AntigenTest(ANTIsens),mod)
+	lines(x,q,col=cols[i])
+	z = x[q==min(q)]
+	abline(v=z)
+	text(z,0,paste(z))
+}
+
+frame(x)
+title("Omicron variant PCR test")
+for (i in 1:length(prior))
+{
+	p = prior[i]
+	q = PostProbTransmission(x,p,PcrTest(PCRsens),mod)
+	lines(x,q,col=cols[i])
+	z = x[q==min(q)]
+	abline(v=z)
+	text(z,0,paste(z))
+}
+
+x = (0:100)/4
+par(mfrow=c(2,1))
+
+mod = DMod
+frame(x)
+title("Delta variant")
+base=ProbInfected(x,0,Unconditional,mod)
+lines(x,base,col="green")
+
+y=ProbInfected(x,1,Unconditional,mod)
+lines(x,y,col="black")
+#z=min(x[y<base])
+#abline(v=z,col="black")
+#text(z,0,paste(z))
+
+y=ProbInfected(x,1,NoSymptoms,mod)
+lines(x,y,col="blue")
+z=min(x[y<base])
+abline(v=z,col="blue")
+text(z,0,paste(z))
+
+y=ProbInfected(x,1,NoSymptoms*AntigenTest(ANTIsens),mod)
+lines(x,y,col="red")
+z=min(x[y<base])
+abline(v=z,col="red")
+text(z,0,paste(z))
+
+y=ProbInfected(x,1,NoSymptoms*PcrTest(PCRsens),mod)
+lines(x,y,col="orange")
+z=min(x[y<base])
+abline(v=z,col="orange")
+#text(z,0,paste(z))
+
+mod = OMod
+frame(x)
+title("Omicron variant")
+base=ProbInfected(x,0,Unconditional,mod)
+lines(x,base,col="green")
+
+y=ProbInfected(x,1,Unconditional,mod)
+lines(x,y,col="black")
+#z=min(x[y<base])
+#abline(v=z,col="black")
+#text(z,0,paste(z))
+
+y=ProbInfected(x,1,NoSymptoms,mod)
+lines(x,y,col="blue")
+z=min(x[y<base])
+abline(v=z,col="blue")
+text(z,0,paste(z))
+
+y=ProbInfected(x,1,NoSymptoms*AntigenTest(ANTIsens),mod)
+lines(x,y,col="red")
+z=min(x[y<base])
+abline(v=z,col="red")
+text(z,0,paste(z))
+
+y=ProbInfected(x,1,NoSymptoms*PcrTest(PCRsens),mod)
+lines(x,y,col="orange")
+z=min(x[y<base])
+abline(v=z,col="orange")
+#text(z,0,paste(z))
 
